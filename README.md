e75e3ef0755c6e836bdf3161e46f6da4e0d55790
Matheus Braga <matheus@gocache.com.br>
2022-05-27 15:50:10 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,233 @@
 # gocache-discovery-ingress-plugin
 API Discovery plugin for ingress
+
+# Envoriment variables neeeded
+
+## GOCACHE_DISCOVERY_TOKEN
+
+Authentication token, you cna get it from GoCache support chat or in the api discovery section in the panel
+
+## GOCACHE_DISCOVERY_ADDERESS 
+
+IP Adderess where is located the service, currently (beta): "129.159.62.11"
+
+## GOCACHE_DISCOVERY_HOSTNAME (optional)
+
+Default to api-inventory.gocache.com.br
+# Installation
+
+## Using helm
+
+You need to add an `extraInitContainers` directive to your helm config:
+```yaml
+extraInitContainers:
+  - image: k8s.gcr.io/git-sync/git-sync:v3.1.7
+    name: init-clone-plugins
+    imagePullPolicy: Always
+    volumeMounts:
+    - name: lua-plugins
+      mountPath: /plugins
+    env:
+    - name: GIT_SYNC_REPO
+      value: "https://github.com/gocachebr/gocache-discovery-ingress-plugin.git"
+    - name: GIT_SYNC_BRANCH
+      value: "main"
+    - name: GIT_SYNC_ROOT
+      value: "/plugins"
+    - name: GIT_SYNC_DEST
+      value: "gocache"
+    - name: GIT_SYNC_ONE_TIME
+      value: "true"
+    - name: GIT_SYNC_DEPTH
+      value: "1"
+```
+
+Then mount the volumes for the plugin:
+```yaml
+  extraVolumes:
+  - name: lua-plugins
+    emptyDir: {}
+  extraVolumeMounts:
+  - name: lua-plugins
+    mountPath: /etc/nginx/lua/plugins
+```
+Add another section for the envs:
+```yaml
+  extraEnvs:
+  - name: GOCACHE_DISCOVERY_TOKEN
+    value: "<yourtoken>"
+  - name: GOCACHE_DISCOVERY_ADDERESS
+    value: "129.159.62.11"
+  - name: GOCACHE_DISCOVERY_HOSTNAME
+  	value: api-inventory.gocache.com.br
+```
+And finally enable the plugin:
+```yaml
+config:
+    plugins: gocache
+    lua-shared-dicts: "gocache: 30M"
+```
+
+Here's the full controller lets say you named it `values.yaml`:
+```yaml
+controller:
+  extraEnvs:
+  - name: GOCACHE_DISCOVERY_TOKEN
+    value: "<yourtoken>"
+  - name: GOCACHE_DISCOVERY_ADDERESS
+    value: "129.159.62.11"
+  - name: GOCACHE_DISCOVERY_HOSTNAME
+  	value: api-inventory.gocache.com.br
+  extraVolumes:
+  - name: lua-plugins
+    emptyDir: {}
+  extraInitContainers:
+  - image: k8s.gcr.io/git-sync/git-sync:v3.1.7
+    name: init-clone-plugins
+    imagePullPolicy: Always
+    volumeMounts:
+    - name: lua-plugins
+      mountPath: /plugins
+    env:
+    - name: GIT_SYNC_REPO
+      value: "https://github.com/gocachebr/gocache-discovery-ingress-plugin.git"
+    - name: GIT_SYNC_BRANCH
+      value: "main"
+    - name: GIT_SYNC_ROOT
+      value: "/plugins"
+    - name: GIT_SYNC_DEST
+      value: "gocache"
+    - name: GIT_SYNC_ONE_TIME
+      value: "true"
+    - name: GIT_SYNC_DEPTH
+      value: "1"
+  extraVolumeMounts:
+  - name: lua-plugins
+    mountPath: /etc/nginx/lua/plugins
+  config:
+    plugins: gocache
+    lua-shared-dicts: "gocache: 30M"
+```
+
+Once done, you can apply it:
+```bash
+helm install --version 4.1.2 ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx -f values.yaml
+```
+OR
+```bash
+helm upgrade ingress-nginx -f values.yaml ingress-nginx/ingress-nginx -n ingress-nginx
+```
+
+## Using baremetal containers
+
+inside your deploy.yaml that usually are provided by ingress itself, there is a section for `Deployment`. In that section you must create the volumes, set some envoriment variables and add an `initContainer` for the plugin code itself.
+
+Here's all the fields needed:
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  ...
+spec:
+  ...
+  template:
+    ...
+    spec:
+      initContainers:
+      - image: k8s.gcr.io/git-sync/git-sync:v3.1.7
+        name: init-clone-plugins
+        imagePullPolicy: Always
+        volumeMounts:
+        - name: lua-plugins
+          mountPath: /plugins
+        env:
+        - name: GIT_SYNC_REPO
+          value: "https://github.com/gocachebr/gocache-discovery-ingress-plugin.git"
+        - name: GIT_SYNC_BRANCH
+          value: "main"
+        - name: GIT_SYNC_ROOT
+          value: "/plugins"
+        - name: GIT_SYNC_DEST
+          value: "gocache"
+        - name: GIT_SYNC_ONE_TIME
+          value: "true"
+        - name: GIT_SYNC_DEPTH
+          value: "1"
+      containers:
+      - args:
+        ...
+        env:
+        ...
+        - name: GOCACHE_DISCOVERY_ADDERESS
+          value: "129.159.62.11"
+        - name: GOCACHE_DISCOVERY_HOSTNAME
+  		  value: api-inventory.gocache.com.br
+        - name: GOCACHE_DISCOVERY_TOKEN
+          value: "<yourtoken>"
+        ...
+        volumeMounts:
+        ...
+        - name: lua-plugins
+          mountPath: /etc/nginx/lua/plugins
+      ...
+      volumes:
+      - name: webhook-cert
+        secret:
+          secretName: ingress-nginx-admission
+      - name: lua-plugins
+        emptyDir: {}
+```
+
+## Config map method
+
+There is an alternative method for installing the plugin using the config map instead of the git-sync method.
+To make it simple, follow the previous steps but remove all the section from `initContainer` or `extraInitContainers`
+
+Then you need to adjust the volumes accordingly
+
+### HELM volumes
+
+```yaml
+extraVolumes:
+  - name: gocache-plugin
+    configMap:
+      name: gocache-plugin
+  extraVolumeMounts:
+  - name: gocache-plugin
+    mountPath: /etc/nginx/lua/plugins/gocache/
+```
+
+### Baremetal volumes
+
+```yaml
+	volumeMounts:
+    - name: gocache-plugin
+      mountPath: /etc/nginx/lua/plugins/gocache/
+volumes:
+- name: gocache-plugin
+  configMap:
+  name: gocache-plugin
+```
+
+### Config map itself
+
+Now, you can create an file called `gocache-plugin.yaml` and add the following in it:
+```yaml
+apiVersion: v1
+kind: ConfigMap
+data:
+  main.lua: |
+  	<plugin code here>
+metadata:
+  name: gocache-plugin
+  namespace: ingress-nginx
+
+```
+
+Note that is needed to copy the content of the main.lua to the section highlighted in the config map, otherwise you will get an error or empty config file. Another important thing is that you might need to change the namespace accordinly to whats been setup previously.
+
+Once done, you can apply the config
+```bash
+kubectl apply -f gocache-plugin.yaml
+```
+
---
+++ main.lua
@@ -0,0 +1,295 @@
+local cjson = require("cjson")
+local http = require("resty.http")
+local ngx = ngx
+
+local token = os.getenv("GOCACHE_DISCOVERY_TOKEN")
+local discovery_host  = os.getenv("GOCACHE_DISCOVERY_HOSTNAME")
+
+if not discovery_host or discovery_host == "" then 
+   discovery_host = "api-inventory.gocache.com.br"
+end
+
+local discovery_adderess  = os.getenv("GOCACHE_DISCOVERY_ADDERESS")
+
+if not discovery_adderess or discovery_adderess == "" then 
+   discovery_adderess = "0.0.0.0"
+end
+
+local gcshared = ngx.shared.gocache
+
+local inventory_max_body_size = 2048
+
+local accepted_request_content_types = {
+    "application/json",
+    "application/x%-www%-form%-urlencoded",
+    "text/plain"
+}
+
+local accepted_response_content_types = {
+   "application/json",
+   "application/xml",
+   "text/xml"
+}
+
+local ignore_headers = {
+    ["a-im"]=true,
+    ["accept"]=true,
+    ["accept-charset"]=true,
+    ["accept-datetime"]=true,
+    ["accept-encoding"]=true,
+    ["accept-language"]=true,
+    ["access-control-request-method"]=true,
+    ["access-control-request-headers"]=true,
+    ["cache-control"]=true,
+    ["connection"]=true,
+    ["content-encoding"]=true,
+    ["content-length"]=true,
+    ["content-md5"]=true,
+    ["content-type"]=true,
+    ["cookie"]=true,
+    ["date"]=true,
+    ["expect"]=true,
+    ["forwarded"]=true,
+    ["from"]=true,
+    ["host"]=true,
+    ["http2-settings"]=true,
+    ["if-match"]=true,
+    ["if-modified-since"]=true,
+    ["if-none-match"]=true,
+    ["if-range"]=true,
+    ["if-unmodified-since"]=true,
+    ["max-forwards"]=true,
+    ["origin"]=true,
+    ["pragma"]=true,
+    ["prefer"]=true,
+    ["proxy-authorization"]=true,
+    ["range"]=true,
+    ["referer"]=true,
+    ["sec-fetch-dest"]=true,
+    ["sec-fetch-mode"]=true,
+    ["sec-fetch-site"]=true,
+    ["sec-fetch-user"]=true,
+    ["te"]=true,
+    ["trailer"]=true,
+    ["transfer-encoding"]=true,
+    ["user-agent"]=true,
+    ["upgrade"]=true,
+    ["via"]=true,
+    ["warning"]=true,
+    ["upgrade-insecure-requests"]=true,
+    ["x-requested-with"]=true,
+    ["dnt"]=true,
+    ["x-forwarded-for"]=true,
+    ["x-forwarded-host"]=true,
+    ["x-forwarded-proto"]=true,
+    ["front-end-https"]=true,
+    ["x-http-method-override"]=true,
+    ["x-att-deviceid"]=true,
+    ["x-wap-profile"]=true,
+    ["proxy-connection"]=true,
+    ["x-uidh"]=true,
+    ["x-csrf-token"]=true,
+    ["x-request-id"]=true,
+    ["x-correlation-id"]=true,
+    ["save-data"]=true,
+}
+
+local _M = {}
+
+local function send_api_discovery_request(premature, request_info, token)
+   if premature then
+       return
+   end
+
+   local httpc = http.new()
+
+   local ok,err = httpc:connect(discovery_adderess, 443)
+   if not ok then
+       err = err or ""
+       ngx.log(ngx.ERR,"Error while connecting to api_inventory for " .. cjson.encode(discovery_adderess) .. " : " .. err)
+       return
+   end
+
+   local ok, err = httpc:ssl_handshake(nil, discovery_host,false)
+   if not ok then
+       err = err or ""
+       ngx.log(ngx.ERR,"Error while doing ssl handshake to api_inventory for " .. cjson.encode(discovery_adderess) .. " -> "..cjson.encode(discovery_host).." : " .. err)
+       return
+   end
+
+   local res,err = httpc:request({
+       path = "/discover/push",
+       method = "POST",
+       body = cjson.encode(request_info),
+       headers = {
+           ["Content-Type"] = "application/json",
+           ["GoCache-Inventory-Toke"] = token,
+       }
+   })
+
+   httpc:close()
+   if not res then
+       err = err or ""
+       ngx.log(ngx.ERR,"Error while sending request to api_inventory for " .. cjson.encode(discovery_adderess) .. " -> "..cjson.encode(discovery_host) .. " : " .. err)
+       return
+   end   
+
+   ngx.log(ngx.INFO, "Status code from sending "..(#request_info).." requests for discovery: "..res.status)                  
+end
+
+local function collect_cookie(collector, content)
+   content = content .. ';'
+   for segment in content:gmatch("(.-);") do 
+      local name = segment:match("[%s%t]*(.-)=.+")
+      if name then
+         collector[name] = ""
+      end
+   end
+end
+
+local function obfuscate_cookies(params)
+   local final_cookies = {}
+
+   if type(params) == 'table' then 
+      for __, data in pairs(params) do 
+         collect_cookie(final_cookies,  data)
+      end
+   else 
+      collect_cookie(final_cookies, params)
+   end
+ 
+   return final_cookies
+end
+
+
+local function obfuscate_headers(params)
+   local final_headers = {}
+   for key, content in pairs(params) do 
+      if not ignore_headers[key] then
+         if type(content) == 'table' then 
+            for __, ___ in pairs(content) do 
+               final_headers[key] = ""
+            end
+         else 
+            final_headers[key] = ""
+         end
+      end
+   end
+   return final_headers
+end
+
+local function obfuscate_parameters(params)
+
+   local check_type = {}
+            
+   check_type["table"] = function()
+      if params[1] ~= nil then
+         local arrElement = params[1]
+         params = nil
+         params = {obfuscate_parameters(arrElement)}
+      else
+         for k, v in pairs(params) do
+            params[k] = obfuscate_parameters(v)
+         end
+      end
+      return params
+   end
+   check_type["string"] = function()
+       return ""
+   end
+   check_type["boolean"] = function()
+       return false
+   end
+   check_type["number"] = function()
+       return 0
+   end
+
+   return check_type[type(params)]()
+end
+
+
+function _M.log()
+
+   local response_headers = ngx.resp.get_headers()
+   local res_content_type = response_headers["content_type"]
+
+   local add = false
+   if tonumber(ngx.var.status) == 200 and res_content_type ~= nil then
+      for _, ct in ipairs(accepted_response_content_types) do
+         if res_content_type:match(ct) then
+            add = true 
+         end
+      end
+   end
+
+   if add then
+      local request_headers = ngx.req.get_headers()
+
+      local req_content_type = request_headers.content_type
+
+      local body_data
+      for _, ct in ipairs(accepted_request_content_types) do
+          if req_content_type:match(ct) then
+              local raw_body_data = ngx.req.get_body_data()
+              if raw_body_data ~= nil and #raw_body_data < inventory_max_body_size then
+
+                  local success, jsonData = pcall(cjson.decode, raw_body_data) 
+                  if success then
+                      body_data = jsonData
+                  else
+                      body_data = ngx.req.get_post_args()
+                  end
+                  break
+              end
+          end
+      end
+      local body_info
+
+      if body_data ~= nil then
+          local obfuscated_body_data = obfuscate_parameters(body_data)
+          body_info = cjson.encode(obfuscated_body_data)
+      end
+
+      local cookie_data
+      local cookies = request_headers['cookie']
+      if cookies then 
+         cookie_data = obfuscate_cookies(cookies)
+      end
+               
+      local request_info = {
+         hostname = ngx.var.http_host,
+         uri = ngx.var.request_uri,
+         status = ngx.status,
+         method = ngx.var.request_method,
+         res_content_type = res_content_type,
+         req_content_type = req_content_type,
+         body_data = body_info,
+         header_data = obfuscate_headers(request_headers),
+         cookie_data = cookie_data,
+      }
+
+      gcshared:lpush("requests", cjson.encode(request_info))
+
+      local last_sent = gcshared:get("last_sent")
+      last_sent = tonumber(last_sent) or 0
+
+      if last_sent <= ngx.now() or gcshared:llen("requests") >= 50 then 
+         
+         gcshared:set("last_sent", ngx.now()+60)
+         local content = {}
+         for i=1, 50 do 
+            local req_info = gcshared:rpop("requests")
+            if req_info then 
+               local req_data = cjson.decode(req_info)
+               content[#content+1] = req_data
+            end
+         end
+         if #content > 0 then 
+            ngx.timer.at(1, send_api_discovery_request, content, token)
+         end
+      end
+   end
+end
+
+
+return _M
